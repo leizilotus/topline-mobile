e5d18a8beb2b86851488b82de0dfffff707b719f
lotus <leizilotus@163.com>
2019-07-10 23:06:35 +0800

    适配rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+
+const { sep } = require('path')
+
+module.exports = ({ file }) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1 ? 37.5 : 75
+
+  return {
+    plugins: {
+      autoprefixer: {
+        browsers: ['Android >= 4.0', 'iOS >= 7']
+      },
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
   </div>
 </template>
 
@@ -16,5 +12,9 @@ export default {
 </script>
 
 <style scoped>
-
+.box {
+  width: 750px;
+  height: 200px;
+  background-color: blueviolet
+}
 </style>
