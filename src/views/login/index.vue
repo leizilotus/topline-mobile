e3c08634bf0eb88392259bf7daf75c484e9c79ca
lotus <leizilotus@163.com>
2019-07-12 17:51:42 +0800

    表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,12 +9,14 @@
             clearable
             label="手机号"
             placeholder="请输入手机号"
+            :error-message="errors.mobile"
         />
         <van-field
             v-model="user.code"
             type="password"
             label="密码"
             placeholder="请输入密码"
+            :error-message="errors.code"
             required
         />
         </van-cell-group>
@@ -24,6 +26,7 @@
             class="login-btn"
             @click.prevent="handleLogin"
             :loading="loginLoading"
+            loading-text="登录中..."
           >登录</van-button>
         </div>
     </form>
@@ -40,7 +43,11 @@ export default {
         mobile: '15120025791',
         code: '246810'
       },
-      loginLoading: false
+      loginLoading: false.name,
+      errors: {
+        mobile: '',
+        code: ''
+      }
     }
   },
 
@@ -50,8 +57,24 @@ export default {
 
   methods: {
     async handleLogin () {
-      this.loginLoading = true
       try {
+        // 发送之前，验证表单数据，校验通过，才能进行登录
+        const { mobile, code } = this.user
+        const errors = this.errors
+        if (mobile.length) {
+          errors.mobile = ''
+        } else {
+          errors.mobile = '手机号不能为空'
+          return
+        }
+        if (code.length) {
+          errors.code = ''
+        } else {
+          errors.code = '验证码不能为空'
+          return
+        }
+        this.loginLoading = true
+
         const data = await login(this.user)
         this.$store.commit('setUser', data)
         // 目前粗暴的将页面跳转到首页
