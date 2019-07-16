133f1820811f33db80818544e9e9ae38dc85800b
lotus <leizilotus@163.com>
2019-07-16 13:22:22 +0800

    首页文章列表-封装接口Api

--- src/api/article.js
+++ src/api/article.js
@@ -1,3 +1,21 @@
 /**
  * 文章相关接口模块
  */
+
+import request from '@/utils/request'
+
+export const getArticles = ({
+  channelId,
+  timestamp,
+  withTop
+}) => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_1/articles',
+    params: {
+      channel_id: channelId,
+      timestamp,
+      with_top: withTop
+    }
+  })
+}
